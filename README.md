8277a7cc6b2b2904fd1c5bc67499b9431bb408f2
codeonium <codeon2020@gmail.com>
2021-07-14 13:44:32 +0100

    Understanding imports and alias in Go

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Golang-Training-Project
 Training project to understand the workings of Golang
+
+
+To Run:
+go run main.go
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	t "time"
+	// creates a alias for time, also imports can be done inside (), no need for , or ;
+)
+// standard library for packages to import can be found at https://pkg.go.dev/std
 
 func main () {
+	// main function does not need to be called, it will run when the program is called
 	fmt.Println("Let's see if this works, also know as Hello World!")
+	fmt.Println(t.Now())
 }
